4e4705f3b33b6ad4be89764c5883e56b339b3717
akshay <akshaysharma5432@gmail.com>
2023-08-19 14:25:19 -0400

    common wordpress functions

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+function sample(){
+  echo 2+2;
+}
+sample();
+?>
+
+<!-- function to get the site'sname already defined -->
+<h1><?php bloginfo('name'); ?></h1>
+<h1><?php bloginfo('description'); ?></h1>
