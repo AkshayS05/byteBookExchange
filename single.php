982e6345670d39eb4852dad67629fc7b750afab5
akshay <akshaysharma5432@gmail.com>
2023-08-19 14:57:36 -0400

    create clickable posts and single detailed posts

--- single.php
+++ single.php
@@ -1,12 +1,14 @@
+
+<!-- This is a convention in wordpress to creaste a file name single.php which will hold the single page details whenever use visits any of the exisiting posts -->
 <?php
 // this is a wordpress function to loop until there are posts
 while(have_posts()) {
 // this function will keep track of all the posts
   the_post(); ?>
 <!-- to geenrate links we use the_permalink() -->
-  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  <h2> <?php the_title(); ?></h2>
   <?php the_content(); ?>
-
+  <hr>
 <?php }
 
 ?>
