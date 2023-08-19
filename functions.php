78bb474a8ff209626a8aa6def6ca4566a0fdae7f
akshay <akshaysharma5432@gmail.com>
2023-08-19 18:37:14 -0400

    add images' path using wordpress function

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ function bbe_files(){
   wp_enqueue_style('bbe_main_styles', get_theme_file_uri('./build/style-index.css') );
   wp_enqueue_style('bbe_extra_styles', get_theme_file_uri('./build/index.css') );
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
 }
 // wp_enqueue_scrips - to load css and js files
 // this will be called when wordpress has to on a precise time        
