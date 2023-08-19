85e1cb5d5f2e3155c113e7360a180babdc35911d
akshay <akshaysharma5432@gmail.com>
2023-08-19 18:27:56 -0400

    add css styles for site and font-awesome icons

--- functions.php
+++ functions.php
@@ -2,7 +2,9 @@
 
 function bbe_files(){
   //load as many css and js files using this function
-  wp_enqueue_style('bbe_main_styles', get_stylesheet_uri());
+  wp_enqueue_style('bbe_main_styles', get_theme_file_uri('./build/style-index.css') );
+  wp_enqueue_style('bbe_extra_styles', get_theme_file_uri('./build/index.css') );
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
 }
 // wp_enqueue_scrips - to load css and js files
 // this will be called when wordpress has to on a precise time        
