f4b67ffc19b8cddfeb5ab74b6b4219401ea55fb6
akshay <akshaysharma5432@gmail.com>
2023-08-19 19:11:04 -0400

    importd javascript file in functions

--- functions.php
+++ functions.php
@@ -6,6 +6,9 @@ function bbe_files(){
   wp_enqueue_style('bbe_extra_styles', get_theme_file_uri('./build/index.css') );
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
+  //javascript file
+  wp_enqueue_script('main-bbe-js', get_theme_file_uri('./build/index.js'), array('jquery'),'1.0',true );
+
 }
 // wp_enqueue_scrips - to load css and js files
 // this will be called when wordpress has to on a precise time        
