f16c51ab043a2aa28ffd28634222816c40508bcd
akshay <akshaysharma5432@gmail.com>
2023-08-19 20:38:45 -0400

    add dynamic site pages

--- functions.php
+++ functions.php
@@ -10,7 +10,14 @@ function bbe_files(){
   wp_enqueue_script('main-bbe-js', get_theme_file_uri('./build/index.js'), array('jquery'),'1.0',true );
 
 }
+
+function university_features(){
+  add_theme_support('title-tag');
+}
+
 // wp_enqueue_scrips - to load css and js files
 // this will be called when wordpress has to on a precise time        
 add_action('wp_enqueue_scripts', "bbe_files");
+//to get the site title
+add_action('after_setup_theme', "university_features");
 ?>
--- page.php
+++ page.php
@@ -5,11 +5,41 @@ get_header();
 // this is a wordpress function to loop until there are pages
 while(have_posts()) {
 // this function will keep track of all the posts
-  the_post(); ?>
-  <h1>A page not a post</h1>
-<!-- to geenrate links we use the_permalink() -->
-  <h2> <?php the_title(); ?></h2>
-  <?php the_content(); ?>
+ 
+?>
+
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p>Replace it later please.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+        </p>
+      </div>
+<!-- sidebar menu -->
+<!--
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <ul class="min-list">
+          <li class="current_page_item"><a href="#">Our History</a></li>
+          <li><a href="#">Our Goals</a></li>
+        </ul>
+      </div>
+-->
+      <div class="generic-content">
+    <?php 
+    the_content();
+    ?>
+      </div>
+    </div>
 
 <?php }
 
