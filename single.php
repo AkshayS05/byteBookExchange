3766b86e66ac7e0ba507dcd67fc384c24d83ff19
akshay <akshaysharma5432@gmail.com>
2023-08-20 20:54:54 -0400

    display single post content and banner

--- single.php
+++ single.php
@@ -6,10 +6,23 @@ get_header();
 while(have_posts()) {
 // this function will keep track of all the posts
   the_post(); ?>
-<!-- to geenrate links we use the_permalink() -->
-  <h2> <?php the_title(); ?></h2>
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p>Replace it later please.</p>
+        </div>
+      </div>
+    </div>
+    <div class="container container--narrow page-section">
+    Metabox will go here.
+    <div class="generic-content">
   <?php the_content(); ?>
-  <hr>
+</div>
+  </div>
+
+
 <?php }
 get_footer();
 ?>
