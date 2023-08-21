39bc14a55dca3ceb4c323fd9712693b0f504157d
akshay <akshaysharma5432@gmail.com>
2023-08-20 21:13:57 -0400

    display author details and date posted on single posts

--- single.php
+++ single.php
@@ -16,7 +16,12 @@ while(have_posts()) {
       </div>
     </div>
     <div class="container container--narrow page-section">
-    Metabox will go here.
+
+    <div class="metabox metabox--position-up metabox--with-home-link">
+          <!-- get_the_title to get the title of the post we provide and as a link we use get_permalink -->
+      <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main"> Posted By <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?> </span></p>
+    </div>
+
     <div class="generic-content">
   <?php the_content(); ?>
 </div>
