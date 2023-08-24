afc3cc49cdd497f442a69e2b5c00754be97baa9f
akshay <akshaysharma5432@gmail.com>
2023-08-23 23:52:14 -0400

    add custom event dates with php function

--- front-page.php
+++ front-page.php
@@ -29,8 +29,14 @@
             <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
               <!-- event_date is the name we provided in plugin acf -->
-              <span class="event-summary__month"><?php the_field('event_date'); ?></span>
-              <span class="event-summary__day"><?php ?></span>
+              <span class="event-summary__month"><?php
+              // php class --> it will return the current date if we don't provide it any date.
+              $eventDate = new DateTime(get_field('event_date'));
+              echo $eventDate->format('M')
+              ?></span>
+              <span class="event-summary__day"><?php 
+               echo $eventDate->format('d')
+              ?></span>
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
