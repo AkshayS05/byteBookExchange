787aec3282ad9b03937e59dc70af365f545f3b0d
akshay <akshaysharma5432@gmail.com>
2023-08-25 14:25:12 -0400

    display related events on programs page

--- single-program.php
+++ single-program.php
@@ -23,8 +23,71 @@ while(have_posts()) {
     </div>
 
     <div class="generic-content">
-  <?php the_content(); ?>
-</div>
+      
+      <?php the_content(); ?>
+    </div>
+    <!-- to display related events -->
+
+<?php 
+          $today = date('Ymd');
+          $homePageEvents= new WP_Query(array(
+            //parameters
+            //-1 means give us all the posts that meet this condition
+            'posts_per_page' => 2,
+            'post_type' => 'event',
+            // to say we weant to sort using custom data
+            'meta_key' => 'event_date',
+            //for letters and values, we can use meta_value, however, for numbers, we should user meta_value_num
+            'orderby' => 'meta_value_num',
+            'order'=> 'ASC',
+            //to not show past events
+           
+            'meta_query' => array(
+              array(
+                //only return posts if the event date is greater or equal to todays date.
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              ),
+              array(
+                // if the array of related programs contain the ID number of the current program post
+                'key' => 'related_programs',
+                'compare' => 'LIKE',
+                // to get the string
+                'value' => '"' . get_the_ID() . '"'
+              )
+            )
+          )); 
+
+          while($homePageEvents ->have_posts()){
+            //this will make the data, ready
+            $homePageEvents->the_post(); ?>
+
+            <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+              <!-- event_date is the name we provided in plugin acf -->
+              <span class="event-summary__month"><?php
+              // php class --> it will return the current date if we don't provide it any date.
+              $eventDate = new DateTime(get_field('event_date'));
+              echo $eventDate->format('M')
+              ?></span>
+              <span class="event-summary__day"><?php 
+               echo $eventDate->format('d')
+              ?></span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+              <!-- wp_trim_words get two arguments, one the content we want to trim and second is number of words we want to trim. -->
+              <?php if(has_excerpt()){
+                // the_excerpt(); will create an automatic padding on each side, thus to tackle that we can use another function called get_the_excerpt()
+                  echo get_the_excerpt();
+              } else{
+                echo wp_trim_words(get_the_content(), 18);
+              } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+          <?php } ?>
   </div>
 
 
