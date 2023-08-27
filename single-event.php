dc6bc12363575e1aca80d72abed5e323150bcb32
akshay <akshaysharma5432@gmail.com>
2023-08-27 14:52:56 -0400

    make banners and titles dynamic for all pages

--- single-event.php
+++ single-event.php
@@ -1,20 +1,13 @@
-
 <!-- This is a convention in wordpress to create a file name single.php which will hold the single page details whenever use visits any of the exisiting posts -->
 <?php
 get_header();
 // this is a wordpress function to loop until there are posts
 while(have_posts()) {
 // this function will keep track of all the posts
-  the_post(); ?>
-<div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p>Replace it later please.</p>
-        </div>
-      </div>
-    </div>
+  the_post(); 
+  pageBanner();
+  ?>
+
     <div class="container container--narrow page-section">
 
     <div class="metabox metabox--position-up metabox--with-home-link">
