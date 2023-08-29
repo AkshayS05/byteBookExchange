b79dac8bc216ce9f7dd28b5fde94d4da2df27eb4
akshay <akshaysharma5432@gmail.com>
2023-08-29 00:48:11 -0400

    add campuses' locationon map

---
+++ archive-campus.php
@@ -0,0 +1,30 @@
+<?php
+get_header();
+pageBanner(array(
+  'title'=> "Our Campuses",
+  'subtitle'=> "Checkout our virtual campuses which actually doesn't exist 🏫",
+
+));
+?>
+
+      <div class="container container--narrow page-section">
+        <ul class="link-list min-list">
+        <?php
+        // will loop until there are posts in the database
+        while(have_posts()){
+          the_post(); ?>
+    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+    <?php the_field('map_location'); ?>
+        <?php
+        }
+        // <!-- pagination -->
+        echo paginate_links();
+        ?>
+        </ul>
+
+      </div>
+      
+      <?php
+     the_field('Map');
+get_footer();
+?>
