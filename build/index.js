69c1aa5641e2b865ec023270795a44ae0682b77e
akshay <akshaysharma5432@gmail.com>
2023-09-02 11:40:29 -0400

    display relevant information upon search

--- build/index.js
+++ build/index.js
@@ -148,19 +148,51 @@ class Search {
   }
   //this will run after 2000ms
   getResults() {
-    jquery__WEBPACK_IMPORTED_MODULE_0___default().when(jquery__WEBPACK_IMPORTED_MODULE_0___default().getJSON(`${bbeData.root_url}/wp-json/wp/v2/posts?search=${this.searchField.val()}`), jquery__WEBPACK_IMPORTED_MODULE_0___default().getJSON(`${bbeData.root_url}/wp-json/wp/v2/pages?search=${this.searchField.val()}`)).then((posts, pages) => {
-      let combinedResults = posts[0].concat(pages[0]);
+    //ist argument--where we wana send req
+    jquery__WEBPACK_IMPORTED_MODULE_0___default().getJSON(`${bbeData.root_url}/wp-json/bbe/v1/search?term=${this.searchField.val()}`, results => {
       this.resultsDiv.html(`
-  <h2 class="search-overlay__section-title">General Information</h2>
-  ${combinedResults.length ? ' <ul class="link-list min-list">' : "<p>No general information matches that search ☹"}
-  ${combinedResults.map(post => `<li><a href="${post.link}">${post.title.rendered}</a>${post.type === "post" ? ` by ${post.authorName}` : ""}</li>
-        ${combinedResults.length ? "</ul>" : ""}
-        `).join("")}
-  </ul>
-  `);
+        <div class="row">
+        <div class="one-third">
+        <h2 class="search-overlay__section-title">General Information</h2>
+        ${results.generalInfo.length ? ' <ul class="link-list min-list">' : "<p>No general information matches that search ☹"}
+      
+        ${results.generalInfo.map(post => `<li><a href="${post.permalink}">${post.title}</a>${post.type === "post" ? ` by ${post.authorName}` : ""}</li>`).join("")}
+        ${results.generalInfo.length.length ? "</ul>" : ""} 
+        </div>
+
+
+        
+        <div class="one-third">
+        <h2 class="search-overlay__section-title">Programs</h2>
+        ${results.programs.length ? ' <ul class="link-list min-list">' : `<p>No programs match that search. <a href="${bbeData.root_url}/programs">View all programs</a></p>`}
+      
+  ${results.programs.map(post => `<li><a href="${post.permalink}">${post.title}</a></li>`).join("")}
+        ${results.programs.length ? "</ul>" : ""} 
+        <h2>Instructors</h2>
+        ${results.instructors.length ? '<ul class="professor-cards">' : `<p>No instructors match that search.</p>`}
+          ${results.instructors.map(item => `
+            <li class="professor-card__list-item">
+              <a class="professor-card" href="${item.permalink}">
+                <img class="professor-card__image" src="${item.image}">
+                <span class="professor-card__name">${item.title}</span>
+              </a>
+            </li>
+          `).join("")}
+        ${results.instructors.length ? "</ul>" : ""}
+        </div>
+
+        <div class="one-third">
+        <h2 class="search-overlay__section-title">Campuses</h2>
+        ${results.campuses.length ? ' <ul class="link-list min-list">' : `<p>No campuses match that search ☹ <a href="${bbeData.root_url}/campuses"> View all Campuses</a> </p>`}
+      
+  ${results.campuses.map(post => `<li><a href="${post.permalink}">${post.title}</a></li>`).join("")}
+        ${results.campuses.length.length ? "</ul>" : ""} 
+        
+        <h2>Events</h2>
+        </div>
+        </div>
+      `);
       this.isSpinnerVisible = false;
-    }, () => {
-      this, this.resultsDiv.html("<p>Unexpected error; Please try again!</p>");
     });
   }
   keyPressDispatcher(e) {
--- includes/search-route.php
+++ includes/search-route.php
@@ -13,24 +13,58 @@ function bbeSearchResults($data) {
   // wordpress automatically converts php data to json data.
   // to return json
   // here we got collection of posts
-  $instructors = new WP_Query(array(
-    'post_type' => 'instructor',
+  $mainQuery = new WP_Query(array(
+    'post_type' => array('post','page','instructor', 'program', 'campus', 'event'),
     // s= search 
     // prevention from sql injection
     's' =>  sanitize_text_field($data['term']) 
   ));
   //posts = where all data for posts live
 
-    $instructorResults = array();
+    $mainQueryResults = array(
+      'generalInfo' => array(),
+      'instructors' => array(),
+      'programs' => array(),
+      'events' => array(),
+      'campuses' => array(),
+    );
 
-    while($instructors->have_posts()){
-      $instructors -> the_post();
+    while($mainQuery ->have_posts()){
+      $mainQuery -> the_post();
       // this function takes two arguments, one is array you want to add on to, what you want to add in that array 
-      array_push($instructorResults, array(
-        'title' => get_the_title(),
-        'permalink' => get_the_permalink()
-      ));
+      if(get_post_type() == 'post'OR get_post_type() == 'page'){
+        array_push($mainQueryResults['generalInfo'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink(),
+          'postType' => get_post_type(),
+          'authorName' => get_the_author()
+        ));
+      }
+      if(get_post_type() == 'instructor'){
+        array_push($mainQueryResults['instructors'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
+      if(get_post_type() == 'program'){
+        array_push($mainQueryResults['programs'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
+      if(get_post_type() == 'campus'){
+        array_push($mainQueryResults['campuses'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
+      if(get_post_type() == 'event'){
+        array_push($mainQueryResults['events'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
     }
-    return $instructorResults;
+    return $mainQueryResults;
 }
 ?>
