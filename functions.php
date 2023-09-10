fe050599782c58f6f59ee8309bda4f2c4294dc4e
akshay <akshaysharma5432@gmail.com>
2023-09-10 14:14:09 -0400

    configure for deplyment

--- functions.php
+++ functions.php
@@ -52,6 +52,14 @@ function bbe_files(){
   wp_enqueue_style('bbe_main_styles', get_theme_file_uri('./build/style-index.css') );
   wp_enqueue_style('bbe_extra_styles', get_theme_file_uri('./build/index.css') );
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+  if (strstr($_SERVER['SERVER_NAME'], 'bbe.local')) {
+    // Enqueue the JavaScript file for local development
+    wp_enqueue_script('main-bbe-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+} else {
+    // Enqueue a different JavaScript file for production or other environments
+    wp_enqueue_script('main-bbe-js', get_theme_file_uri('./build/production-bundled.js'), NULL, '1.0', true);
+}
+
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
   //javascript file
   wp_enqueue_script('main-bbe-js', get_theme_file_uri('./build/index.js'), array('jquery'),'1.0',true );
